b1efdb032cd7d37ccf353502105f539ac16f65a3
WangYinchuan <yinchuan@tinoq.com>
2019-11-20 20:11:47 +0800

    first commit

--- README.md
+++ README.md
@@ -1,22 +1,38 @@
-# isync 同步工具
-文件同步备份
+# mv2s3 上传工具
+上传指定文件夹的内容到 s3， 并删除已上传的文件
 
-监视指定目录下文件的新增，并将新增文件同步到指定目标目录
+**说明**
+运行的时候，会把指定的目录重命名为新的临时目录，同时以指定目录的原名创建一个空目录；
+上传完成后会删除临时目录
 
 ## 安装
-`npm install -g @tinoq/isync`
+`npm install -g @tinoq/mv2s3`
 
 ## 运行
-查看帮助
 
-`isync -h`
+### s3:
+`mv2s3 -s /src -d a/b/c -i AccessKeyId -k AccessSecretKey -b wyc`
+
+### minio:
+`mv2s3 -s /src -d a/b/c -i AccessKeyId -k AccessSecretKey -b wyc -e endpoint -m true`
+
+> 可以通过 crontab 定时运行
+
+## 查看帮助
+
+`mv2s3 -h`
 
 ```
-Usage: isync [options]
+Usage: mv2s3 [options]
 
 Options:
-  -V, --version      output the version number
-  -s, --src <path>   【必要】源路径
-  -d, --dest <path>  【必要】目标路径
-  -h, --help         output usage information
+  -V, --version            output the version number
+  -s, --src <path>         【必要】源路径
+  -d, --dest <path>        【必要】目标路径
+  -i, --id <string>        【必要】aws_access_key_id
+  -k, --key <string>       【必要】aws_secret_access_key
+  -b, --bucket <string>    【必要】bucket
+  -e, --endpoint <string>  endpoint, eg. http://127.0.0.1:9000
+  -m, --minio <boolean>    use minio (default: false)
+  -h, --help               output usage information
 ```
--- app.js
+++ app.js
@@ -3,84 +3,144 @@
 const fs = require('fs')
 const fsPromises = require('fs').promises
 const path = require('path')
-const shell = require('shelljs')
-const chokidar = require('chokidar')
+const AWS = require('aws-sdk')
 const commander = require('commander')
 const pkg = require('./package')
 
-console.log('cwd', process.cwd())
+console.log('cwd', process.cwd(), 'pid', process.pid)
 const program = new commander.Command()
 program.version(pkg.version)
 program
   .requiredOption('-s, --src <path>', '【必要】源路径')
   .requiredOption('-d, --dest <path>', '【必要】目标路径')
-
+  .requiredOption('-i, --id <string>', '【必要】aws_access_key_id')
+  .requiredOption('-k, --key <string>', '【必要】aws_secret_access_key')
+  .requiredOption('-b, --bucket <string>', '【必要】bucket')
+  .option('-e, --endpoint <string>', 'endpoint, eg. http://127.0.0.1:9000')
+  .option('-m, --minio <boolean>', 'use minio', false)
+  
 program.parse(process.argv)
 
-if (!program.src) {
-  console.info('请指定源目录，使用： -s --src')
-  process.exit(1)
-}
-if (!program.dest) {
-  console.info('请指定目标目录，使用： -d --dest')
-  process.exit(1)
-}
-
+console.log('begin:', new Date())
 start()
 
 async function start(){
-  console.info('running...')
+  const s3 = new AWS.S3({
+    endpoint: program.endpoint,
+    apiVersion: '2006-03-01',
+    accessKeyId: program.id,
+    secretAccessKey: program.key,
+    httpOptions:{
+      timeout: 300000
+    },
+    s3ForcePathStyle: program.minio, // needed with minio?
+    // signatureVersion: 'v4'
+  });
+  // 检查 bucket 是否存在，线上账号无权查看，所以取消
+  /* try{
+    await checkBucket(program.bucket, s3)
+  }catch(e){
+    console.log('bucket 不存在')
+    process.exit(1)
+  } */
+  let srcDirPath = path.resolve(process.cwd(), program.src)
   try{
-    let srcDirPath = path.resolve(process.cwd(), program.src)
-    let destDirPath = path.resolve(process.cwd(), program.dest)
-    console.info(`src:${srcDirPath}\ndest:${destDirPath}`)
-    try{
-      await fsPromises.access(srcDirPath)
-    }catch(e){
-      console.info('源目录不存在或无权访问',e)
-      process.exit(1)
-    }
-    try{
-      await fsPromises.access(destDirPath, fs.constants.W_OK)
-    }catch(e){
-      console.info('目标目录不存在或无权访问',e)
-      process.exit(1)
-    }
-    let srcStat = await fsPromises.stat(srcDirPath)
-    let destStat = await fsPromises.stat(destDirPath)
-    if (!srcStat.isDirectory()) {
-      console.info('源路径必须为文件夹')
-      process.exit(1)
-    }
-    if (!destStat.isDirectory()) {
-      console.info('目标路径必须为文件夹')
-      process.exit(1)
-    }
+    await fsPromises.access(srcDirPath)
+  }catch(e){
+    console.log('源路径不存在或者无权访问')
+    process.exit(1)
+  }
+  let srcStat = await fsPromises.stat(srcDirPath);
+  if(!srcStat.isDirectory()){
+    console.log('源路径不是文件夹')
+    process.exit(1)
+  }
 
-    const srcDirWatcher = chokidar.watch(srcDirPath, {
-      persistent: true,
-      ignoreInitial: true
-    })
-    srcDirWatcher
-      .on('add', async (filePath)=>{
-        console.info('监视到新增文件:', filePath)
-        let fileRelativePath = path.relative(srcDirPath, filePath)
-        let destFileFullPath = path.resolve(destDirPath, fileRelativePath)
-        try{
-          let fileStat = await fsPromises.stat(filePath)
-          let destFileDirPath = path.dirname(destFileFullPath)
-          await fsPromises.mkdir(destFileDirPath,{recursive:true})
-          let rs = shell.cp('-rf', filePath, destFileFullPath)
-          await fsPromises.utimes(destFileFullPath, fileStat.atime, fileStat.mtime) //保留时间戳
-          console.info(` ${filePath} 到 ${destFileFullPath} ${rs.stderr?'失败['+rs.stderr+']':'成功'}!`)
-        }catch(e){
-          console.info('备份失败:', e)
-        }
-      })
-      console.log('监视中...')
+  try{
+    //将文件夹更名，同时以原名创建新的文件夹
+    let tempPath = path.resolve(`${srcDirPath}_temp_${process.pid}_${new Date().getTime()}`)
+    await fsPromises.rename(srcDirPath, tempPath)
+    console.info(`move src:${srcDirPath} to temp:${tempPath}`)
+    await fsPromises.mkdir(srcDirPath)
+    console.info(`upload local:${tempPath} \nto s3:${program.dest}`)
+    await uploadDir(tempPath, tempPath, s3);
+    console.log('finish:', new Date())
   }catch(e){
     console.info('运行错误：', e)
     console.info('重启...')
-    start()
+    // start()
   }
+}
+
+
+async function uploadDir(rootPath, dir, s3){
+  try{
+    await fsPromises.access(dir)
+  }catch(e){
+    console.log(`${dir} isn't exist, or don't have permission access`)
+    return
+  }
+  let stat = await fsPromises.stat(dir);
+  if(!stat.isDirectory()){
+    console.log(`${dir} isn't directory`)
+    return
+  }
+  let fileNameList = await fsPromises.readdir(dir);
+  for(let filename of fileNameList){
+    let filePath = path.resolve(dir, filename)
+    let fileStat = await fsPromises.stat(filePath)
+    if(fileStat.isDirectory()){
+      await uploadDir(rootPath, filePath, s3)
+    }else{
+      await uploadFile(rootPath, filePath, s3)
+    }
+  }
+  let afterList = await fsPromises.readdir(dir);
+  if(afterList.length==0){
+    await fsPromises.rmdir(dir)
+    console.log('删除空文件夹', dir)
+  }
+}
+
+function uploadFile(rootPath, filePath, s3){
+  return new Promise((resolve, reject)=>{
+    let fileRelativePath = path.relative(rootPath, filePath)
+    let fileStream = fs.createReadStream(filePath)
+    fileStream.on('error', function(err){
+      console.log(`read ${filePath} err:${err}`);
+      reject(err);
+    })
+    let keyString = `${program.dest}/${fileRelativePath}`;
+    console.log(`upload ${filePath} to ${keyString}`)
+    s3.upload({
+      Bucket: program.bucket,
+      Body: fileStream,
+      Key: keyString
+    }).on('httpUploadProgress', function(evt) {
+      console.log(`${filePath}:${evt}`);
+    }).send(async function(err, rs){
+      if(err){
+        reject(err)
+      }else{
+        await fsPromises.unlink(filePath)
+        resolve(rs)
+      }
+    });
+  })
+}
+
+async function checkBucket(bucket, s3){
+  return new Promise((resolve, reject)=>{
+    s3.headBucket({
+      Bucket: bucket
+    }, function(err, data) {
+      if(err){
+        console.log('checkBucket:err:', err); // an error occurred
+        reject(err)
+      }else{
+        console.log('checkBucket:data:',data);           // successful response
+        resolve(data)
+      }
+    });
+  })
 }
